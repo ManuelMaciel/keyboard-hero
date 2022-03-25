7adaefb0f9d12bc8653f78199f31e06f8151522f
ManuelMaciel <manuelmaciel7001@gmail.com>
2022-03-25 20:37:00 -0300

    feat: created components for card and input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Input = () => {
+  return (
+    <div>
+      <p>Hola Mundo</p>
+    </div>
+  )
+}
+
+// Styled Components
+const Input = styled.input`
+
+`
+
+export default Input;
